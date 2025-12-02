b34026251c7c2db33146105f7a8839f2b255f6fc
Saloni Sharma <saloni2019sharma@gmail.com>
2025-12-02 11:40:10 +0530

    How forEach uses consumer interface to consume list

--- src/com/consumer/Main.java
+++ src/com/consumer/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> values = Arrays.asList(4, 5, 6, 7, 8);
         Consumer<Integer> c = new ConsImp();
+        //Check read me to know how to use this consumer in forEach and how it uses accept method internally.
         values.forEach(c);
     }
 }
